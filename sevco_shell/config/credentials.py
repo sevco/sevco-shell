3e704e031d589d2b54f23b000a629c54c5950a36
Nick Murdock <murdock@sevco.io>
2021-07-07 15:54:25 -0500

    Support 'Token' auth

--- sevco_shell/config/credentials.py
+++ sevco_shell/config/credentials.py
@@ -5,8 +5,8 @@ from pathlib import Path
 from typing import List, Optional
 
 import jwt
+import requests
 import toml
-
 from sevco_shell.builders.builder import Builder
 
 
@@ -109,36 +109,46 @@ class CredentialsProviderChain(CredentialsProvider):
         return None
 
 
-class BearerToken:
+class AuthToken:
     def __init__(self, token: str):
         self.token = token
 
-    def jwt(self):
+    def permissions(self, api_host: str) -> List[str]:
+        raise NotImplementedError
+
+    def expired(self) -> bool:
+        raise NotImplementedError
+
+    def validate(self) -> bool:
+        raise NotImplementedError
+
+
+class BearerToken(AuthToken):
+    def __init__(self, token: str):
+        self.token = token
+
+    def _jwt(self):
         # Slice out the 'Bearer ' prefix
         return jwt.decode(self.token[7:], verify=False)
 
-    def permissions(self) -> List[str]:
-        permissions = self.jwt().get('permissions', [])
+    def permissions(self, api_host: str) -> List[str]:
+        permissions = self._jwt().get('permissions', [])
 
-        for r in self.jwt().get('https://sevco/props/permsByRole', []):
+        for r in self._jwt().get('https://sevco/props/permsByRole', []):
             permissions.extend(r['permissions'])
 
         return permissions
 
     def expired(self) -> bool:
-        return time.time() > self.jwt()['exp'] + (5 * 60)
+        return time.time() > self._jwt()['exp'] + (5 * 60)
 
     def validate(self) -> bool:
-        return self.validate_token(self.token)
-
-    @staticmethod
-    def validate_token(token: str) -> bool:
-        if not token.startswith('Bearer '):
+        if not self.token.startswith('Bearer '):
             print("Invalid format.  Expected 'Bearer abcd1234'")
             return False
 
         try:
-            decoded = jwt.decode(token[7:], verify=False)
+            decoded = jwt.decode(self.token[7:], verify=False)
             if time.time() > decoded['exp']:
                 print("Token is expired")
                 return False
@@ -149,11 +159,45 @@ class BearerToken:
         return True
 
 
+class ApiToken(AuthToken):
+    def __init__(self, token: str):
+        self.token = token
+        self._permissions: List[str] = []
+
+    def permissions(self, api_host: str) -> List[str]:
+        if not self._permissions:
+            resp = requests.get(
+                f"{api_host}/v1/admin/user/me/apikey/{self.token[6:]}/claims", headers={"Authorization": self.token})
+            resp.raise_for_status()
+
+            self._permissions = []
+
+            for r in resp.json().get('claims', {}).get('https://sevco/props/permsByRole', []):
+                self._permissions.extend(r['permissions'])
+
+        return self._permissions
+
+    def expired(self) -> bool:
+        return False
+
+    def validate(self) -> bool:
+        return True
+
+
+def auth_token(token: str) -> AuthToken:
+    if token.startswith("Bearer "):
+        return BearerToken(token)
+    elif token.startswith("Token "):
+        return ApiToken(token)
+    else:
+        raise Exception("Token must start with Bearer or Token")
+
+
 class ApiCredentials:
     def __init__(self, provider: CredentialsProvider):
         self.provider = provider
         self._api_host: Optional[str] = None
-        self._bearer_token: Optional[BearerToken] = None
+        self._auth_token: Optional[AuthToken] = None
 
     @property
     def api_host(self) -> str:
@@ -173,53 +217,52 @@ class ApiCredentials:
 
     @property
     def auth_token(self) -> str:
-        return self.bearer_token.token
+        return self._get_auth_token().token
 
-    @property
-    def bearer_token(self) -> BearerToken:
+    def _get_auth_token(self) -> AuthToken:
         # If we don't have a token yet try to get one from the credentials provider
-        if not self._bearer_token:
-            auth_token = self.provider.auth_token()
-            if auth_token:
-                self._bearer_token = BearerToken(auth_token)
+        if not self._auth_token:
+            token = self.provider.auth_token()
+            if token:
+                self._auth_token = auth_token(token)
 
         # If we still don't have a token yet ask the user to provide one
-        if not self._bearer_token or not BearerToken.validate_token(self._bearer_token.token):
-            self._bearer_token = self.auth_token_from_user()
+        if not self._auth_token:
+            self._auth_token = self.auth_token_from_user()
 
             if Builder.get_yes_no("Save API Token to ~/.sevco/credentials?", default_yes=True):
-                self.persist(auth_token=self._bearer_token.token)
+                self.persist(auth_token=self._auth_token.token)
 
         # Make sure any existing tokens are still valid
-        if not self._bearer_token.validate():
-            self._bearer_token = self.auth_token_from_user()
+        if not self._auth_token.validate():
+            self._auth_token = self.auth_token_from_user()
 
             if Builder.get_yes_no("Save API Token to ~/.sevco/credentials?", default_yes=True):
-                self.persist(auth_token=self._bearer_token.token)
+                self.persist(auth_token=self._auth_token.token)
 
         # Make sure the token we are using has not expired
-        if self._bearer_token.expired():
+        if self._auth_token.expired():
             print("API Token has expired")
-            self._bearer_token = self.auth_token_from_user()
+            self._auth_token = self.auth_token_from_user()
 
             if Builder.get_yes_no("Save API Token to ~/.sevco/credentials?", default_yes=True):
-                self.persist(auth_token=self._bearer_token.token)
+                self.persist(auth_token=self._auth_token.token)
 
-        return self._bearer_token
+        return self._auth_token
 
     def permissions(self) -> List[str]:
-        return self.bearer_token.permissions()
+        return self._get_auth_token().permissions(self.api_host)
 
-    def auth_token_from_user(self) -> BearerToken:
+    def auth_token_from_user(self) -> AuthToken:
         import readline  # fix for truncated input on osx
 
-        prompt = "Please provide the Sevco API Bearer Token (format: Bearer abcd1234)"
-        auth_token = Builder.get_input(prompt, required=False)
+        prompt = "Please provide the Sevco API Bearer Token (format: Bearer or Token)"
+        token = Builder.get_input(prompt, required=False)
 
-        while not auth_token or not BearerToken.validate_token(auth_token):
-            auth_token = Builder.get_input(prompt, required=False)
+        while not token or not auth_token(token).validate():
+            token = Builder.get_input(prompt, required=False)
 
-        return BearerToken(auth_token)
+        return auth_token(token)
 
     def persist(self, api_host: Optional[str] = None, auth_token: Optional[str] = None):
         FileCredentialsProvider.persist(api_host, auth_token)
